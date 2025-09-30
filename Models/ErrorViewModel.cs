6437c0f903a782d818cba9a1c4181e5435512ab9
Mr Anthony <44789325+AnthonyDisasi@users.noreply.github.com>
2025-09-30 21:58:27 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EComApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
